b47ed408ae49a86ce4536a9dfa25486cc74c8d35
Sebastian Müller <sebastian.mueller@freiheit.com>
2017-08-13 16:34:35 +0200

    Einfaches Routing zur Anlage von Boxen.

--- src/python/ch/cmce/litterbox-server/litterbox-server-flask.py
+++ src/python/ch/cmce/litterbox-server/litterbox-server-flask.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 import os
 import socket
+import json
+import sys
 
 app = Flask(__name__)
+box = None
 
 @app.route("/")
 def hello():
@@ -10,5 +15,27 @@ def hello():
            "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
+@app.route("/box/<boxID>/", methods=['GET', 'POST'])
+def getOrRegisterBox(boxID):
+    print >> sys.stderr, "getOrRegisterBox: request.is_json={}, request.method={}".format(request.is_json, request.method)
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            return jsonify(registerBox(boxID, data))
+        else:
+            return "Please send JSON (Content-Type: application/json)", 400
+    else:
+        return jsonify(getBox(boxID))
+
+def registerBox(boxID, data):
+    print >> sys.stderr, "registerBox {}: {}".format(boxID, data)
+    global box
+    box = data
+    return box
+
+def getBox(boxID):
+    return box
+
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
